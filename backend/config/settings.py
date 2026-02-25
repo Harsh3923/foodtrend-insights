fa44732d0ea82488591a7991f42c72ef3514d5ed
Harsh <h16patel@torontomu.ca>
2026-02-25 05:10:54 +0000

    updated settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 
     #third-party
     "rest_framework",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,7 +58,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "config.urls"
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
