e9bb3a18766182afb66620c96a4de1b1c9307366
Harsh <h16patel@torontomu.ca>
2026-02-28 20:46:05 +0000

    updated urls.py to accomodate new url for cuisine ranking

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from posts import views
+from .views import api_trending_cuisines
 
 urlpatterns = [
+    path("api/trending-cuisines", api_trending_cuisines),
     path("api/trends/", views.api_trends),
     path("api/search/", views.api_search),
     path("api/posts/", views.api_posts),
